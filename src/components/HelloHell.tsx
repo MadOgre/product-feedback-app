a71965952b1585ae5d005f10294dd5ae3b864b2a
Victoria Kario <victoriakario@users.noreply.github.com>
2023-06-21 01:50:10 -0400

    Update src/components/HelloHell.tsx
    Reduce Eeees by 2.
    
    Co-authored-by: Michael Semko <msemko@gmail.com>

--- src/components/HelloHell.tsx
+++ src/components/HelloHell.tsx
@@ -1,7 +1,7 @@
 export const HelloHell = (): JSX.Element => {
   return (
     <>
-      <h1>Hell. I am in HEEEEEEEllll. Hello!!!!</h1>
+      <h1>Hell. I am in HEEEEEllll. Hello!!!!</h1>
       <p>THis is the most annoying thing in the world.</p>
     </>
   );
