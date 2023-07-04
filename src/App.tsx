47eb477ddeeab66244c5ac614f8217d6f9c4657e
Michael Semko <msemko@gmail.com>
2023-07-04 02:54:16 -0400

    move everything inside mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
+import { HelloHell } from "components/HelloHell";
+import { MobileLayout } from "components/MobileLayout";
 import { Shelf, Spacer } from "components/common/layout";
 import { FrontendMentorMobileText, NoFeedbackMobileText } from "components/common/typography";
-
-import { HelloHell } from "components/HelloHell";
-
-import { LogoArea } from "components/LogoArea";
-
+import { debounce } from "utils/debounce";
 
 function App(): JSX.Element {
+
+  const func1 = debounce(() => console.log("works"), 2000);
+
+
   return (
     <div>
-      <LogoArea />
-      <HelloHell />
-      <FrontendMentorMobileText color="white">
-        This is frontend mentor text
-      </FrontendMentorMobileText>
-      <NoFeedbackMobileText>This is no feedback text</NoFeedbackMobileText>
-      <Shelf>
-        <div style={{background: "red", width: "20px", height: "20px"}} />
-        <div style={{background: "green", width: "20px", height: "20px"}} />
-        <Spacer />
-        <div style={{background: "blue", width: "20px", height: "20px"}} />
-      </Shelf>
+      <MobileLayout>
+        <HelloHell />
+        <FrontendMentorMobileText color="white">
+          This is frontend mentor text
+        </FrontendMentorMobileText>
+        <NoFeedbackMobileText>This is no feedback text</NoFeedbackMobileText>
+        <Shelf>
+          <div style={{background: "red", width: "20px", height: "20px"}} />
+          <div style={{background: "green", width: "20px", height: "20px"}} />
+          <Spacer />
+          <div style={{background: "blue", width: "20px", height: "20px"}} />
+          <button
+            onClick={func1}
+            type="button"
+          >
+            click me
+          </button>
+        </Shelf>
+        <HelloHell />
+        <HelloHell />
+        <HelloHell />
+        <HelloHell />
+        <HelloHell />
+        <HelloHell />
+      </MobileLayout>
     </div>
   );
 }
